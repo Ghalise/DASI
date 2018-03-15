ff38282eb50edab84033f98a6bb6bb88d8ed6e24
LENOVO <LENOVO@10.43.0.232>
2018-03-15 10:30:25 +0100

    Ajout des services clients

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -10,8 +10,10 @@ import dao.ClientDao;
 import dao.EmployeeDao;
 import dao.JpaUtil;
 import dao.MediumDao;
+import entite.AstroProfile;
 import entite.Employee;
 import entite.Medium;
+import java.util.Collection;
 import javax.persistence.RollbackException;
 /**
  *
@@ -23,6 +25,8 @@ public class Service {
     public Service(){}
     
     //**************** SERVICES CLIENT ****************
+    
+    //Service pour enregistrer un nouveau client dans l'application
     public void createClient(Client c)
     {
         JpaUtil.creerEntityManager();
@@ -41,6 +45,7 @@ public class Service {
        
     }
     
+    //Service pour qu'un client puisse se connecter dans l'application
     public Client connectClient(String mail, String password){
         JpaUtil.creerEntityManager();
         ClientDao cd= new ClientDao();
@@ -52,16 +57,20 @@ public class Service {
         return c;
     }
     
-    //service utilisé par un employé pour avoir accès aux informations concernant un client
-    public Client findClient (long id){
-        //pas de try catch ?
-        JpaUtil.creerEntityManager();
-        ClientDao cd= new ClientDao();
-        Client c=cd.find(id);
-        JpaUtil.fermerEntityManager();
-        return c;
+    //Service pour que le client puisse accéder à son profil Astrologique
+    public AstroProfile displayAstroprofile (Client c){
+        return c.getAstroProfile();
     }
     
+    //Service pour que le client puisse consulter la liste des Médiums
+    public Collection<Medium> displayMediums (Client c){
+        JpaUtil.creerEntityManager();
+        MediumDao md = new MediumDao();
+        Collection<Medium> m = md.findAll();
+        JpaUtil.fermerEntityManager();
+        return m;
+    }
+        
     //**************** SERVICES EMPLOYE ****************
     public void createEmployee(Employee emp)
     {
@@ -97,6 +106,16 @@ public class Service {
         }
         JpaUtil.fermerEntityManager();
         return e;
+    }
+    
+    //Service utilisé pour avoir accès aux informations concernant un client
+    public Client findClient (long id){
+        //pas de try catch ?
+        JpaUtil.creerEntityManager();
+        ClientDao cd= new ClientDao();
+        Client c=cd.find(id);
+        JpaUtil.fermerEntityManager();
+        return c;
     }
     
     private void sendMailSuccess(Client c){
